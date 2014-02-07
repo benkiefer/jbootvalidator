7235362c72581795a903a7d5422970a45a8cce56
Ben Kiefer <kieferfam@msn.com>
2014-02-06 22:10:39 -0600

    cleanup and remove classes and help-block when required issue is resolved

--- dist/jBootValidator.js
+++ dist/jBootValidator.js
@@ -1,11 +1,15 @@
 (function ($) {
     $.fn.jBootValidator = function () {
         return this.find('input.form-control').bind('keyup', function (e) {
-            var val = $(this).val();
-			if($(this).attr('required')){
-                if (val == null || val == ''){
-                    $(this).after('<span class="help-block">This field is required.</span>')
+            var $input = $(this),
+                val = $input.val();
+
+			if($input.attr('required')){
+                if (val == null || val.trim() == ''){
+                    $input.after('<span class="help-block">This field is required.</span>')
                         .parent('.form-group').addClass('has-error')
+                } else {
+                    $input.closest('.form-group').removeClass('has-error').find('span.help-block').remove();
                 }
             }
         });
--- test/RequiredSpec.js
+++ test/RequiredSpec.js
@@ -19,6 +19,21 @@ describe('jbootvalidator(required)', function () {
         expect(formGroup.hasClass('has-error')).toBe(true);
     });
 
+    it('should add has-error class when required input is blank including spaces', function () {
+        var formControl = formControlInput(true)
+            .val('      ');
+        var formGroup = formGroupDiv()
+            .append(formControl);
+
+        form.append(formGroup);
+
+        form.jBootValidator();
+
+        formControl.trigger('keyup');
+
+        expect(formGroup.hasClass('has-error')).toBe(true);
+    });
+
     it('should add help-block after form control when required input is blank', function () {
         var formGroup = formGroupDiv();
         var formControl = formControlInput(true);
@@ -29,41 +44,84 @@ describe('jbootvalidator(required)', function () {
 
         formControl.trigger('keyup');
 
-        var $span = form.find('span.help-block');
+        var $span = formGroup.find('span.help-block');
         expect($span.text()).toBe('This field is required.');
     });
 
     it('should not add help-block as a child of the form-group should always be after form-control', function () {
-        var formGroup = formGroupDiv();
-        var colWidthDiv = $('<div>');
         var formControl = formControlInput(true);
-        colWidthDiv.append(formControl);
-        formGroup.append(colWidthDiv);
+        var colWidthDiv = $('<div>')
+            .append(formControl);
+
+        var formGroup = formGroupDiv()
+            .append(colWidthDiv);
+
         form.append(formGroup);
 
         form.jBootValidator();
 
         formControl.trigger('keyup');
 
-        expect(form.children('span.help-block').length).toBe(0);
+        expect(formGroup.children('span.help-block').length).toBe(0);
 
         var $span = colWidthDiv.find('span.help-block');
         expect($span.text()).toBe('This field is required.');
     });
 
-    function formGroupDiv () {
-        var $div = $('<div>');
-        $div.addClass('form-group');
-        return  $div;
+    it('should remove help-block when the required issue is resolved', function () {
+        var formControl = formControlInput(true).val('present');
+
+        var colWidthDiv = $('<div>')
+            .append(formControl)
+            .append(helpBlockSpan());
+
+        var formGroup = formGroupDiv()
+            .append(colWidthDiv);
+
+        form.append(formGroup);
+
+        form.jBootValidator();
+
+        formControl.trigger('keyup');
+
+        expect(formGroup.find('span.help-block').length).toBe(0);
+    });
+
+    it('should remove has-error when the required issue is resolved', function () {
+        var formControl = formControlInput(true)
+            .val('present');
+
+        var colWidthDiv = $('<div>')
+            .append(formControl)
+            .append(helpBlockSpan());
+
+        var formGroup = formGroupDiv()
+            .addClass('has-error')
+            .append(colWidthDiv);
+
+        form.append(formGroup);
+
+        form.jBootValidator();
+
+        formControl.trigger('keyup');
+
+        expect(formGroup.hasClass('has-error')).toBe(false);
+    });
+
+    function formGroupDiv() {
+        return $('<div>').addClass('form-group');
     }
 
-    function formControlInput (required) {
-        var $input = $('<input>');
-        $input.addClass('form-control');
+    function formControlInput(required) {
+        var $input = $('<input>').addClass('form-control');
         if (required) {
             $input.attr('required', true);
         }
         return $input;
+    }
+
+    function helpBlockSpan() {
+        return $('<span>').addClass('help-block');
     }
 
     afterEach(function () {
