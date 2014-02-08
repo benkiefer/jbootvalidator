e6cee2c6387c7bb12f12163c487f9d9296171791
Ben Kiefer <kieferfam@msn.com>
2014-02-07 21:58:08 -0600

    finish up pattern validation. refactor time

--- src/js/jBootValidator.js
+++ src/js/jBootValidator.js
@@ -2,15 +2,35 @@
     'use strict';
 
     $.fn.jBootValidator = function () {
+        function createHelpBlock(text) {
+            return '<span class="help-block jbootval">' + text + '</span>';
+        }
+
+        function doesntHaveHelpBlock(el) {
+            return el.find('span.help-block.jbootval').length === 0;
+        }
+
         return this.find('input.form-control').bind('keyup', function (e) {
             var $input = $(this),
                 $formGroup = $input.closest('.form-group'),
-                val = $input.val();
+                val = $input.val(),
+                pattern = $input.attr('pattern'),
+                title = $input.attr('title');
 
-			if($input.attr('required')){
-                if (val === null || val.trim() === ''){
-                    if ($formGroup.find('span.help-block.jbootval').length === 0) {
-                        $input.after('<span class="help-block jbootval">This field is required.</span>');
+            if ($input.attr('required')) {
+                if (val === null || val.trim() === '') {
+                    if (doesntHaveHelpBlock($formGroup)) {
+                        $input.after(createHelpBlock('This field is required.'));
+                        $formGroup.addClass('has-error');
+                    }
+                } else {
+                    $formGroup.removeClass('has-error').find('span.help-block.jbootval').remove();
+                }
+            }
+            if (pattern) {
+                if (!new RegExp(pattern).test(val)) {
+                    if (doesntHaveHelpBlock($formGroup)) {
+                        $input.after(createHelpBlock(title));
                         $formGroup.addClass('has-error');
                     }
                 } else {
---
+++ test/PatternSpec.js
@@ -0,0 +1,165 @@
+describe('jbootvalidator(pattern)', function () {
+    var form;
+
+    beforeEach(function () {
+        form = $('<form>');
+        $(document.body).append(form);
+    });
+
+    it('should add has-error class when pattern does not match', function () {
+        var formControl = formControlInput('\\d+', '');
+
+        var formGroup = formGroupDiv()
+            .append(formControl);
+
+        form.append(formGroup)
+            .jBootValidator();
+
+        formControl.trigger('keyup');
+
+        expect(formGroup.hasClass('has-error')).toBe(true);
+    });
+
+    it('should add help-block after form control when required input is blank', function () {
+        var formControl = formControlInput('\\d+', 'my title');
+        var formGroup = formGroupDiv()
+            .append(formControl);
+
+        form.append(formGroup)
+            .jBootValidator();
+
+        formControl.trigger('keyup');
+
+        var $span = formGroup.find('span.help-block.jbootval');
+        expect($span.text()).toBe('my title');
+    });
+
+    it('should only add help-block once regardless of number of keyups', function () {
+        var formControl = formControlInput('\\d+', 'my title');
+        var formGroup = formGroupDiv()
+            .append(formControl);
+
+        form.append(formGroup)
+            .jBootValidator();
+
+        formControl.trigger('keyup');
+        formControl.trigger('keyup');
+
+        var $span = formGroup.find('span.help-block.jbootval');
+        expect($span.length).toBe(1);
+    });
+
+    it('should not add help-block as a child of the form-group should always be after form-control', function () {
+        var formControl = formControlInput('\\d+', 'my title');
+        var colWidthDiv = $('<div>')
+            .append(formControl);
+
+        var formGroup = formGroupDiv()
+            .append(colWidthDiv);
+
+        form.append(formGroup)
+            .jBootValidator();
+
+        formControl.trigger('keyup');
+
+        expect(formGroup.children('span.help-block.jbootval').length).toBe(0);
+
+        var $span = colWidthDiv.find('span.help-block.jbootval');
+        expect($span.text()).toBe('my title');
+    });
+
+    it('should not add help-block as a child of the form-group should always be after form-control', function () {
+        var formControl = formControlInput('\\d+', 'my title');
+        var colWidthDiv = $('<div>')
+            .append(formControl);
+
+        var formGroup = formGroupDiv()
+            .append(colWidthDiv);
+
+        form.append(formGroup)
+            .jBootValidator();
+
+        formControl.trigger('keyup');
+
+        expect(formGroup.children('span.help-block.jbootval').length).toBe(0);
+
+        var $span = colWidthDiv.find('span.help-block.jbootval');
+        expect($span.text()).toBe('my title');
+    });
+
+    it('should remove help-block when the required issue is resolved', function () {
+        var formControl = formControlInput('\\d+', 'my title').val('9');
+
+        var colWidthDiv = $('<div>')
+            .append(formControl)
+            .append(helpBlockSpan());
+
+        var formGroup = formGroupDiv()
+            .append(colWidthDiv);
+
+        form.append(formGroup)
+            .jBootValidator();
+
+        formControl.trigger('keyup');
+
+        expect(formGroup.find('span.help-block.jbootval').length).toBe(0);
+    });
+
+    it('should not remove non jbootval help-blocks when the required issue is resolved', function () {
+        var formControl = formControlInput('\\d+', 'my title').val('9');
+
+        var colWidthDiv = $('<div>')
+            .append(formControl)
+            .append($('<span>').addClass('help-block').text('testing'))
+            .append(helpBlockSpan());
+
+        var formGroup = formGroupDiv()
+            .append(colWidthDiv);
+
+        form.append(formGroup)
+            .jBootValidator();
+
+        formControl.trigger('keyup');
+
+        expect(formGroup.find('span.help-block.jbootval').length).toBe(0);
+        expect(formGroup.find('span.help-block').length).toBe(1);
+    });
+
+    it('should remove has-error when the required issue is resolved', function () {
+        var formControl = formControlInput('\\d+', 'my title').val('9');
+
+        var colWidthDiv = $('<div>')
+            .append(formControl)
+            .append(helpBlockSpan());
+
+        var formGroup = formGroupDiv()
+            .addClass('has-error')
+            .append(colWidthDiv);
+
+        form.append(formGroup)
+            .jBootValidator();
+
+        formControl.trigger('keyup');
+
+        expect(formGroup.hasClass('has-error')).toBe(false);
+    });
+
+    function formGroupDiv() {
+        return $('<div>').addClass('form-group');
+    }
+
+    function formControlInput(pattern, title) {
+        return $('<input>').addClass('form-control')
+            .attr('pattern', pattern)
+            .attr('title', title);
+    }
+
+    function helpBlockSpan() {
+        return $('<span>').addClass('help-block.jbootval');
+    }
+
+    afterEach(function () {
+        form.remove();
+        form = null;
+    });
+});
