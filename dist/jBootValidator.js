5470d206903b6e151d059e467a76e6f95a65b11e
Ben Kiefer <kieferfam@msn.com>
2014-02-08 14:27:27 -0600

    added debounce to demo

--- dist/jBootValidator.js
+++ dist/jBootValidator.js
@@ -1,11 +1,27 @@
 (function ($) {
     'use strict';
 
+    if (typeof $.debounce !== 'function') {
+        $.debounce = function (func, wait, immediate) {
+            var timeout;
+            return function () {
+                var context = this, args = arguments;
+                var later = function () {
+                    timeout = null;
+                    if (!immediate) func.apply(context, args);
+                };
+                if (immediate && !timeout) func.apply(context, args);
+                clearTimeout(timeout);
+                timeout = setTimeout(later, wait);
+            };
+        };
+    }
+
     $.fn.jBootValidator = function () {
-        if(typeof String.prototype.trim !== 'function') {
-          String.prototype.trim = function() {
-            return this.replace(/^\s+|\s+$/g, '');
-          };
+        if (typeof String.prototype.trim !== 'function') {
+            String.prototype.trim = function () {
+                return this.replace(/^\s+|\s+$/g, '');
+            };
         }
 
         var invalidPattern = 'This field is invalid.',
@@ -19,36 +35,38 @@
             return el.find('span.help-block.jbootval').length === 0;
         }
 
-        return this
-            .attr('novalidate', 'novalidate')
-            .find('.form-control').bind('keyup focus change', function (e) {
-            var $input = $(this),
-                $formGroup = $input.closest('.form-group'),
-                val = $input.val(),
-                pattern = $input.attr('pattern'),
-                title = $input.attr('title');
 
-            if ($input.attr('required')) {
-                if (val === null || val.trim() === '') {
-                    if (doesntHaveHelpBlock($formGroup)) {
-                        $input.after(createHelpBlock(missingRequired));
-                        $formGroup.addClass('has-error');
+        return this.attr('novalidate', 'novalidate')
+            .find('.form-control')
+            .bind('keyup focus change', $.debounce(function (e) {
+                var $input = $(this),
+                    $formGroup = $input.closest('.form-group'),
+                    val = $input.val(),
+                    pattern = $input.attr('pattern'),
+                    title = $input.attr('title');
+
+                if ($input.attr('required')) {
+                    if (val === null || val.trim() === '') {
+                        if (doesntHaveHelpBlock($formGroup)) {
+                            $input.after(createHelpBlock(missingRequired));
+                            $formGroup.addClass('has-error');
+                        }
+                    } else {
+                        $formGroup.removeClass('has-error').find('span.help-block.jbootval').remove();
                     }
-                } else {
-                    $formGroup.removeClass('has-error').find('span.help-block.jbootval').remove();
                 }
-            }
-            if (pattern) {
-                if (!new RegExp(pattern).test(val)) {
-                    if (doesntHaveHelpBlock($formGroup)) {
-                        $input.after(createHelpBlock(title ? title : invalidPattern));
-                        $formGroup.addClass('has-error');
+                if (pattern) {
+                    if (!new RegExp(pattern).test(val)) {
+                        if (doesntHaveHelpBlock($formGroup)) {
+                            $input.after(createHelpBlock(title ? title : invalidPattern));
+                            $formGroup.addClass('has-error');
+                        }
+                    } else {
+                        $formGroup.removeClass('has-error').find('span.help-block.jbootval').remove();
                     }
-                } else {
-                    $formGroup.removeClass('has-error').find('span.help-block.jbootval').remove();
                 }
-            }
-        });
+            }, 300)
+        );
     };
 }(jQuery));
 
